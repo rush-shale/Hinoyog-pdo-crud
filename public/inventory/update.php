8d8c68e33f1d64d6b198e81cac7c1b88e57457ea
rush-shale <hinoyogjohnrushel@gmail.com>
2024-05-20 18:17:45 +0800

    added initial files

--- public/inventory/update.php
+++ public/inventory/update.php
@@ -1,73 +1,125 @@
 <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, otherwise redirect to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: index.php");
-    exit;
-}
- 
 // Include config file
-require_once "./db/config.php";
+require_once "../../db/config.php";
  
 // Define variables and initialize with empty values
-$new_password = $confirm_password = "";
-$new_password_err = $confirm_password_err = "";
+$product_name = $product_details = $product_retail_price = "";
+$product_name_err = $product_details_err = $product_retail_price_err = "";
  
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Validate new password
-    if(empty(trim($_POST["new_password"]))){
-        $new_password_err = "Please enter the new password.";     
-    } elseif(strlen(trim($_POST["new_password"])) < 6){
-        $new_password_err = "Password must have atleast 6 characters.";
+if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
+    // Get hidden input value
+    $product_id = $_POST["product_id"];
+    
+    // Validate name
+    $input_product_name = trim($_POST["product_name"]);
+    if(empty($input_product_name)){
+        $product_name_err = "Please enter a name.";
+    } elseif(!filter_var($input_product_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $product_name_err = "Please enter a valid name.";
     } else{
-        $new_password = trim($_POST["new_password"]);
+        $product_name = $input_product_name;
     }
     
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm the password.";
+    // Validate product details
+    $input_product_details = trim($_POST["product_details"]);
+    if(empty($input_product_details)){
+        $aproduct_details_err = "Please enter product details.";     
     } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($new_password_err) && ($new_password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
+        $product_details = $input_product_details;
     }
-        
-    // Check input errors before updating the database
-    if(empty($new_password_err) && empty($confirm_password_err)){
+    
+    // Validate product retail price
+    $input_product_retail_price = trim($_POST["product_retail_price"]);
+    if(empty($input_product_retail_price)){
+        $product_retail_price_err = "Please enter the retail price amount.";     
+    } elseif(!ctype_digit($input_product_retail_price)){
+        $product_retail_price_err = "Please enter a positive integer value.";
+    } else{
+        $product_retail_price = $input_product_retail_price;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($product_name_err) && empty($product_details_err) && empty($product_retail_price_err)){
         // Prepare an update statement
-        $sql = "UPDATE users SET password = :password WHERE id = :id";
-        
+        $sql = "UPDATE products SET product_name=:product_name, product_details=:product_details, product_retail_price=:product_retail_price WHERE product_id=:product_id";
+ 
         if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
-            $stmt->bindParam(":id", $param_id, PDO::PARAM_INT);
+            $stmt->bindParam(":product_name", $param_product_name);
+            $stmt->bindParam(":product_details", $param_product_details);
+            $stmt->bindParam(":product_retail_price", $param_product_retail_price);
+            $stmt->bindParam(":product_id", $param_product_id);
             
             // Set parameters
-            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
-            $param_id = $_SESSION["id"];
+            $param_product_name = $product_name;
+            $param_product_details = $product_details;
+            $param_product_retail_price = $product_retail_price;
+            $param_product_id = $product_id;
             
             // Attempt to execute the prepared statement
             if($stmt->execute()){
-                // Password updated successfully. Destroy the session, and redirect to login page
-                session_destroy();
-                header("location: index.php");
+                // Records updated successfully. Redirect to landing page
+                header("location: ../user/dashboard.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
-
-            // Close statement
-            unset($stmt);
         }
+         
+        // Close statement
+        unset($stmt);
     }
     
     // Close connection
     unset($pdo);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["product_id"]) && !empty(trim($_GET["product_id"]))){
+        // Get URL parameter
+        $product_id =  trim($_GET["product_id"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM products WHERE product_id = :product_id";
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":product_id", $param_product_id);
+            
+            // Set parameters
+            $param_product_id = $product_id;
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                if($stmt->rowCount() == 1){
+                    /* Fetch result row as an associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                
+                    // Retrieve individual field value
+                    $product_name = $row["product_name"];
+                    $product_details = $row["product_details"];
+                    $product_retail_price = $row["product_retail_price"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: ../user/error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        unset($stmt);
+        
+        // Close connection
+        unset($pdo);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: ../user/error.php");
+        exit();
+    }
 }
 ?>
  
@@ -75,33 +127,76 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Reset Password</title>
+    <title>Update Record</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
     </style>
 </head>
 <body>
     <div class="wrapper">
-        <h2>Reset Password</h2>
-        <p>Please fill out this form to reset your password.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
-            <div class="form-group">
-                <label>New Password</label>
-                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
-                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link ml-2" href="../db/welcome.php">Cancel</a>
-            </div>
-        </form>
-    </div>    
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Update Record</h2>
+                    <p>Please edit the input values and submit to update the product record.</p>
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="product_name" class="form-control <?php echo (!empty($product_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $product_name; ?>">
+                            <span class="invalid-feedback"><?php echo $product_name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Product Details</label>
+                            <textarea name="product_details" class="form-control <?php echo (!empty($product_details_err)) ? 'is-invalid' : ''; ?>"><?php echo $product_details; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $product_details_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Product Retail Price</label>
+                            <input type="text" name="product_retail_price" class="form-control <?php echo (!empty($product_retail_price_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $product_retail_price; ?>">
+                            <span class="invalid-feedback"><?php echo $product_retail_price_err;?></span>
+                        </div>
+                        
+                        <!--Initialize product id-->
+                        <input type="hidden" name="product_id" value="<?php echo $product_id; ?>"/>
+                        
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#saveUpdateChangesModal">
+                            Save Changes
+                        </button>
+                        
+                        <!-- Modal -->
+                        <div class="modal fade" id="saveUpdateChangesModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Confirmation</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Save changes?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <input type="submit" class="btn btn-primary" value="Submit">
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+
+                        <a href="../user/dashboard.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+
+<!--Additional Bootstrap Dependancies-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
