2eef61d0a9110d576b745fff721def52bd070140
rush-shale <hinoyogjohnrushel@gmail.com>
2024-05-20 18:16:16 +0800

    added initial files

--- public/inventory/delete.php
+++ public/inventory/delete.php
@@ -1,23 +1,23 @@
 <?php
 // Process delete operation after confirmation
-if(isset($_POST["id"]) && !empty($_POST["id"])){
+if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
     // Include config file
-    require_once "../db/config.php";
+    require_once "../../db/config.php";
     
     // Prepare a delete statement
-    $sql = "DELETE FROM employees WHERE id = :id";
+    $sql = "DELETE FROM products WHERE product_id = :product_id";
     
     if($stmt = $pdo->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bindParam(":id", $param_id);
+        $stmt->bindParam(":product_id", $param_product_id);
         
         // Set parameters
-        $param_id = trim($_POST["id"]);
+        $param_product_id = trim($_POST["product_id"]);
         
         // Attempt to execute the prepared statement
         if($stmt->execute()){
             // Records deleted successfully. Redirect to landing page
-            header("location: index.php");
+            header("location: ../user/dashboard.php");
             exit();
         } else{
             echo "Oops! Something went wrong. Please try again later.";
@@ -31,9 +31,9 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     unset($pdo);
 } else{
     // Check existence of id parameter
-    if(empty(trim($_GET["id"]))){
+    if(empty(trim($_GET["product_id"]))){
         // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
+        header("location: ../user/error.php");
         exit();
     }
 }
@@ -60,11 +60,11 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                     <h2 class="mt-5 mb-3">Delete Record</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger">
-                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                            <p>Are you sure you want to delete this employee record?</p>
+                            <input type="hidden" name="product_id" value="<?php echo trim($_GET["product_id"]); ?>"/>
+                            <p>Are you sure you want to delete this record?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary ml-2">No</a>
+                                <a href="../user/dashboard.php" class="btn btn-secondary ml-2">No</a>
                             </p>
                         </div>
                     </form>
