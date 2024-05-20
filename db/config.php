82f77b692467647ec17ffe7e8b39dc5a3796dc93
rush-shale <hinoyogjohnrushel@gmail.com>
2024-05-20 18:10:50 +0800

    added initial files

--- db/config.php
+++ db/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'rushel');
+define('DB_NAME', 'hinoyog');
  
 /* Attempt to connect to MySQL database */
 try{
